070957cbe021ebdf302f6042a8cd0b3d0eff1e47
Unknown <imoz070493@gmail.com>
2019-09-14 16:22:04 -0500

    Laravel V1

---
+++ app/Http/Controllers/PruebaController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PruebaController extends Controller
+{
+    public function index(){
+
+    }
+
+	public function create(){
+    	
+    }
+
+    public function update(){
+    	
+    }
+
+}
