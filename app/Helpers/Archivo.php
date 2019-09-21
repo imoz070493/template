d48be2e0eb72627d3b7c8e337b238325eb0098c0
imoz070493 <imoz070493@gmail.com>
2019-09-21 07:53:38 -0500

    primera version

---
+++ app/Helpers/Archivo.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Helpers;
+
+class Archivo{
+
+	public static function suma($a,$b){
+		return $a+$b;
+	}
+
+	public static function guardarDocumento($document){
+		$exploded = explode(',',$document);
+        $decoded = base64_decode($exploded[1]);
+
+        if(str_contains($exploded[0],'pdf')){
+            $extension = 'pdf';
+        }else{
+            $extension = '';
+        }
+
+        $fileName = str_random().'.'.$extension;
+
+        $path = public_path().'/document/'.$fileName;
+
+        file_put_contents($path, $decoded);
+
+        return $fileName;
+	}
+}
